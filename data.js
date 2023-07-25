fa59c9f50ada3ab54c2e0fe3771e88522b5a7bf9
Gowtham 3ad <dev.krishna.dev.dev@gmail.com>
2023-07-25 16:08:19 +0530

    files added

---
+++ data.js
@@ -0,0 +1,147 @@
+const data = [{
+    code: 1,
+    name: 'Anna University - CEG',
+    address: 'chennai',
+    course: ''
+    ,avgCutOff:192.76,
+},
+{
+    code: 2,
+    name: 'Anna University - ACT',
+    address: 'chennai',
+    course: ''
+    ,avgCutOff:'',
+
+},
+{
+    code: '4',
+    name: 'Anna University - Mit Campus',
+    address: 'chrompet,chennai',
+    course: ''
+    ,avgCutOff:'192.31',
+
+},
+{
+    code: '1315',
+    name: 'SSN',
+    address: 'chennai',
+    course: ''
+    ,avgCutOff:'191.67',
+
+
+},
+{
+    code: '2006',
+    name: 'PSG college of engineering',
+    address: 'peelamedu,Coimbatore',
+    course: ''
+    ,avgCutOff:186.43,
+
+},
+{
+    code: '2377',
+    name: 'PSG Institute of technology',
+    address: 'neelambur,Coimbatore',
+    course: ''
+    ,avgCutOff:'',
+
+},
+{
+    code: '5008',
+    name: 'Thiagaraja',
+    address: 'madurai',
+    course: ''
+    ,avgCutOff:187.75,
+
+},
+{
+    code: '2712',
+    name: 'Kumaraguru',
+    address: 'coimbatore',
+    course: ''
+    ,avgCutOff:'',
+
+},
+{
+    code: '1399',
+    name: 'Chennai Institute of Technology',
+    address: 'Puduper Village, Nandambakkam Post, Kundrathur, Chennai',
+    course: ''
+    ,avgCutOff:'186.33',
+
+},
+{
+    code: '2007',
+    name: 'Coimbatore Institute of Technology (Autonomous)',
+    address: 'Civil Aerodrome Post, Coimbatore',
+    course: ''
+    ,avgCutOff:'188.29',
+
+},
+{
+    code: '2702',
+    name: 'Bannari Amman',
+    address: 'Sathyamanagalam',
+    course: ''
+    ,avgCutOff:'',
+
+},
+
+
+{
+    SN:13,
+    code: '2005',
+    name: 'Government College of Technology (Autonomous)',
+    address: 'Thadagam Road, Coimbatore District',
+    course: ''
+    ,avgCutOff:'183.28',
+
+},
+{
+    SN:'Last',
+    code: '1219',
+    name: 'Sri Venkateswara College of Engineering (Autonomous)',
+    address: 'Irungattukottai S.O., Sriperumbudur Taluk, Kancheepuram',
+    course: ''  
+    ,avgCutOff:'178.97',
+
+},
+{
+    code: '1113',
+    name: 'R M K Engineering College (Autonomous),',
+    address: 'Gummidipoondi, Thiruvallur District ',
+    course: ''  
+    ,avgCutOff:'180.62',
+
+},
+{
+    code: '1211',
+    name: 'Rajalakshmi Engineering College (Autonomous),',
+    address: 'Sriperumpudur Taluk, Kancheepuram District ',
+    course: ''  
+    ,avgCutOff:'178.38',
+},
+{
+    code: '1210',
+    name: 'Panimalar Engineering College (Autonomous), ,',
+    address: 'Nazarethpet, Poonamallee, Chennai ',
+    course: ''  
+    ,avgCutOff:'161.06',
+},
+{
+    code: '2718',
+    name: 'Sri Krishna College of Enginering and Technology (Autonomous)(SKCET)',
+    address: 'Kuniamuthur, Coimbatore',
+    course: ''  
+    ,avgCutOff:'180.12',
+},
+{
+    code: '2722',
+    name: 'Sri Krishna College of Technology (Autonomous) ,(SKCT)',
+    address: ' Kovaipudur Post, Coimbatore',
+    course: ''  
+    ,avgCutOff:'161.06',
+},
+];
+
+export {data};
---
+++ script.js
@@ -0,0 +1,82 @@
+import { data } from "./data.js";
+
+const tableBody = document.getElementById("table-body");
+const addCollegeForm = document.getElementById("add-college-form");
+
+
+const data1 = [{
+    code: 1,
+    name: 'Anna University ',
+    address: 'Coimbatore',
+    course: 'CSE'
+},
+{
+    code: 2,
+    name: 'ssn',
+    address: 'chennai',
+    course: 'CSE'
+},
+];
+
+
+
+for (const college of data) {
+  const row = document.createElement("tr");
+
+  row.innerHTML = `
+   <td>${college.code == undefined ||college.code ==""  ? '':college.code}</td>
+    <td>${college.name == undefined ||college.name =="" ? '':college.name}</td>
+    <td>${college.address == undefined ||college.address =="" ? '':college.address}</td>
+    <td>${college.course == undefined ||college.course =="" ? '':college.course}</td>
+    <td>${college.avgCutOff == undefined ||college.avgCutOff =="" ? '':college.avgCutOff}</td>
+  `;
+
+  tableBody.appendChild(row);
+}
+
+// Add event listeners to the form.
+addCollegeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const code = event.target.elements.namedItem("code").value;
+  const name = event.target.elements.namedItem("name").value;
+  const address = event.target.elements.namedItem("address").value;
+  const course = event.target.elements.namedItem("course").value;
+
+  // Add the college to the data array.
+  data.push({
+    
+    code: code,
+    name: name,
+    address: address,
+    course: course
+  });
+
+  // Clear the form.
+  event.target.reset();
+
+  // Update the table.
+  updateTable();
+});
+
+// Function to update the table.
+function updateTable() {
+  tableBody.innerHTML = "";
+
+  for (const college of data) {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${college.SN}</td>
+      <td>${college.code}</td>
+      <td>${college.name}</td>
+      <td>${college.address}</td>
+      <td>${college.course}</td>
+    `;
+
+    tableBody.appendChild(row);
+    tableBody.classList.add("responsive");
+    // data.push(college);
+
+  }
+}
